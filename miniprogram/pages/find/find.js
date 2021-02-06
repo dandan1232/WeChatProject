697f00178bb60568b0830e8338c23d08747ff679
Danbao <345652127@qq.com>
2021-02-06 16:07:11 +0800

    登录授权组件&弹框组件

--- miniprogram/pages/find/find.js
+++ miniprogram/pages/find/find.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 控制底部弹出层是否显示
+    modalShow:false,
   },
   onSearch(event){
     keyword=event.detail.keyword
@@ -18,57 +19,40 @@ Page({
   onLoad (options) {
   },
   onPublish(){
+    // 获取用户的当前设置，返回值中只会出现小程序已经向用户请求过的权限，
+    // 根据是否具有scope.userInfo属性，判断用户是都授权
+    wx.getSetting({
+      success:(res)=>{
+        console.log('当前设置'+JSON.stringify(res))
+        if(res.authSetting['scope.userInfo']){
+          wx.getUserInfo({
+            success:(res)=>{
+              console.log(res)
+              this.onLoaginSuccess({
+                detail:res.userInfo
+              })
+            }
+          })
+        }else{
+          this.setData({
+            modalShow:true,
+          })
+        }
+      }
+    })
+  },
+  onLoaginSuccess(event){
+    console.log('>>>>>>'+event)
+    const detail=event.detail
+    console.log(detail.nickName)
     wx.navigateTo({
       url: '../publish/publish',
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onLoginFail(){
+    wx.showModal({
+      title: '授权用户才能发布',
+      content:'',
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
