0428b6a00b1286f98d8be27e8710a023921c655d
Danbao <345652127@qq.com>
2021-02-06 21:26:43 +0800

    优化

--- miniprogram/pages/find/find.js
+++ miniprogram/pages/find/find.js
@@ -1,34 +1,23 @@
-let keyword=''//搜索关键字
+let keyword =''
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    // 控制底部弹出层是否显示
-    modalShow:false,
+  data:{
+    modalShow: false,
   },
   onSearch(event){
-    keyword=event.detail.keyword
+    keyword = event.detail.keyword
     console.log(keyword)
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad (options) {
-  },
+  onLoad(options){},
   onPublish(){
-    // 获取用户的当前设置，返回值中只会出现小程序已经向用户请求过的权限，
-    // 根据是否具有scope.userInfo属性，判断用户是都授权
     wx.getSetting({
-      success:(res)=>{
-        console.log('当前设置'+JSON.stringify(res))
-        if(res.authSetting['scope.userInfo']){
+      success: (res) => {
+        console.log('当前设置'+ JSON.stringify(res))
+        if (res.authSetting['scope.userInfo']) {
           wx.getUserInfo({
-            success:(res)=>{
+            lang: 'en',
+            success: (res) =>{
               console.log(res)
-              this.onLoaginSuccess({
+              this.onLoginSuccess({
                 detail:res.userInfo
               })
             }
@@ -41,18 +30,19 @@ Page({
       }
     })
   },
-  onLoaginSuccess(event){
-    console.log('>>>>>>'+event)
-    const detail=event.detail
+  onLoginSuccess(event){
+    console.log(event)
+    const detail = event.detail
     console.log(detail.nickName)
     wx.navigateTo({
-      url: '../publish/publish',
-    })
-  },
-  onLoginFail(){
-    wx.showModal({
-      title: '授权用户才能发布',
-      content:'',
-    })
-  },
+    url:'../publish/publish',
+  })
+},
+onLoginFail(){
+  wx.showModal({
+    title: '授权用户才能发布',
+    content: '',
+  })
+},
+  onLoad(options){},
 })
